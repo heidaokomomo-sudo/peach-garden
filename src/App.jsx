ac4be870bcac8aa806f06b4531a56890bf8f75f7
heidaokomomo-sudo <heidaokomomo@gmail.com>
2026-07-18 08:49:02 +0900

    App.jsx を更新

--- src/App.jsx
+++ src/App.jsx
@@ -85,17 +85,17 @@ const TEAMS = [
 ];
 
 const LISTENERS = [
-  { name: "🐻いし🍑ばっしー🐈", coins: 205650, team: "omurice" },
+  { name: "🐻いし🍑ばっしー🐈", coins: 299070, team: "omurice" },
   { name: "🌸えみか💄❤️🐕", coins: 0, team: "omurice" },
   { name: "はんぺんB_1116", coins: 11420, team: "omurice" },
 
   { name: "のるだん_noru🍺", coins: 7240, team: "chococake" },
   { name: "たろじろさん.", coins: 6300, team: "chococake" },
-  { name: "カシオ_DX", coins: 480, team: "chococake" },
+  { name: "カシオ_DX", coins: 14320, team: "chococake" },
 
-  { name: "ひろぼぉ00", coins: 30, team: "gyoza" },
-  { name: "ツ🍑たきちゃん🍑ツ", coins: 19280, team: "gyoza" },
-  { name: "bababancho🍑", coins: 12370, team: "gyoza" },
+  { name: "ひろぼぉ00", coins: 40, team: "gyoza" },
+  { name: "ツ🍑たきちゃん🍑ツ", coins: 23730, team: "gyoza" },
+  { name: "bababancho🍑", coins: 12860, team: "gyoza" },
   { name: "あっきー🤗🍊🐾", coins: 900, team: "gyoza" },
   { name: "なおやん🚑🌻minami", coins: 300, team: "gyoza" },
 ];
